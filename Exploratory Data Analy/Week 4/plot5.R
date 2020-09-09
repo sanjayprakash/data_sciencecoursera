7aaee70fb4f65f59d7c33085562cccad8c471972
Sanjay Prakash <sanjaystark14@gmail.com>
2020-09-09 13:02:55 +0530

    Update plot5.R

--- Exploratory Data Analy/Week 4/plot5.R
+++ Exploratory Data Analy/Week 4/plot5.R
@@ -7,16 +7,16 @@ download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_d
               , destfile = paste(path, "dataFiles.zip", sep = "/"))
 unzip(zipfile = "dataFiles.zip")
 
-# Load the NEI & SCC data frames.
+
 NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
 SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
 
-# Gather the subset of the NEI data which corresponds to vehicles
+
 vehiclesSCC <- SCC[grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
                    , SCC]
 vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
 
-# Subset the vehicles NEI data to Baltimore's fip
+
 baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
 
 png("plot5.png")
